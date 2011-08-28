c3e6406a6dcf9b698092e6db82be9c23c5d8fb64
Ivan Bessonov <bess2805@mail.ru>
2011-08-28 15:51:08 +0700

    Added saving of some data to localStorage of extension

--- js/dom.js
+++ js/dom.js
@@ -5,6 +5,9 @@ var actions = {
         var rng = selection.getRangeAt(0);
         console.log(path.build(rng.startContainer));
         // returns JSON string with info about selection
+		var rangeStr = range.stringify(rng);
+		console.log(rangeStr);
+		return rangeStr;
     }
 };
 
